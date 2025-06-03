03e5f712a2771640c9d19976f8accb4e60517ad1
Hasan <hasanraza2905@gmail.com>
2025-06-03 15:44:16 +0530

    Otp

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import Toast from "./components/Toast";
 import Star from "./components/Star";
 import Pagination from "./components/Pagination";
+import Otp from "./components/Otp";
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/toast" element={<Toast />} />
         <Route path="/star" element={<Star />} />
-         <Route path="/pagination" element={<Pagination />} />
+        <Route path="/pagination" element={<Pagination />} />
+        <Route path="/otp" element={<Otp />} />
       </Routes>
     </Router>
   );
---
+++ src/components/Otp.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useRef, useState } from "react";
+
+const Otp = () => {
+  const focusRef: any = useRef(null);
+
+  const [curr, setCurr] = useState(1);
+
+  useEffect(() => {
+    if (focusRef.current) {
+      focusRef.current.focus();
+    }
+  }, [curr]);
+
+  const handleChange = (e: any) => {
+    const digits = e.target.value.replace(/\D/g, "");
+    if (digits) {
+      console.log(e.target.selectionStart);
+      e.target.value =
+        e.target.selectionStart <= 1 ? digits[0] : digits.slice(-1);
+      setTimeout(() => e.target.setSelectionRange(1, 1), 0);
+    }
+  };
+
+  const handleKey = (e: any, inp: any) => {
+    if (e.key == "ArrowLeft") setCurr(inp - 1);
+    if (e.key == "ArrowRight") setCurr(inp + 1);
+  };
+
+  return (
+    <div
+      style={{
+        backgroundColor: "black",
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ display: "flex", gap: "1rem" }}>
+        {[1, 2, 3, 4, 5, 6].map((inp) => (
+          <input
+            key={inp}
+            ref={curr == inp ? focusRef : null}
+            style={{ width: "2.5rem", fontSize: "2rem", aspectRatio: "1/1" }}
+            onChange={(e) => {
+              setCurr(inp + 1);
+              handleChange(e);
+            }}
+            onKeyDown={(e) => handleKey(e, inp)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Otp;
